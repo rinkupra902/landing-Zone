22c2c2d399bb6cf15afdfdd45483d3cb9c190c58
Rinku Prajapati <rinkupra902@gmail.com>
2026-09-21 22:49:54 +0530

    bancked setup

--- Terrafrom -1980-infra/Environments/prepord/provider.tf
+++ Terrafrom -1980-infra/Environments/prepord/provider.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "saterraformstate007" 
+    container_name       = "tfstate"
+    key                  =  "preprod.terraform.tfstate"
+}
+}
 provider "azurerm" {
-features {}
+  features {}
 }
